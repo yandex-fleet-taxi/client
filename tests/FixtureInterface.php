94e4ce884d4feda4d7e11a8a0966746de7dfbed7
Valerij Ivashchenko <likemusicdev@gmail.com>
2020-03-11 16:25:55 +0300

    Fix testCreateDriver().

--- tests/FixtureInterface.php
+++ tests/FixtureInterface.php
@@ -2,7 +2,6 @@
 
 namespace Likemusic\YandexFleetTaxiClient\Tests;
 
-
 interface FixtureInterface
 {
     const TEST_DRIVER_DATA = [
@@ -15,26 +14,27 @@ interface FixtureInterface
                 'driver_license' =>
                     [
                         'country' => 'rus',
-                        'number' => '32132132',
-                        'expiration_date' => '2019-09-20',
-                        'issue_date' => '2019-09-01',
-                        'birth_date' => '1985-02-01',
+                        'number' => 'OA111534', //overridden
+                        'expiration_date' => '2021-01-13',
+                        'issue_date' => '2011-01-13',
+//                        'birth_date' => null,
+//                        'birth_date' => '1985-02-01',
                     ],
                 'first_name' => 'Валерий',
                 'last_name' => 'Иващенко',
                 'middle_name' => 'Игоревич',
-                'phones' =>
+                'phones' => //overridden
                     [
-                        '+77533301295',
+                        '+375333012955',
 //                            '+7 (753) 330-12-95',
                     ],
-                'work_rule_id' => 'a6cb3fbe61a54ba28f8f8b5e35b286db',
+                'work_rule_id' => 'bea83f1024f94dfd980a97a26705f070', //overridden
                 'balance_deny_onlycard' => false,
                 'providers' =>
                     [
                         0 => 'yandex',
                     ],
-//                    'hire_date' => '2019-09-01',
+                'hire_date' => '2019-09-01', //overridden
 //                    'deaf' => NULL,
 //                    'email' => NULL,
 //                    'address' => NULL,
