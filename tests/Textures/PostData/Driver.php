382337a5246f1d6cd8e97287843d8ed48f48e8ab
Valerij Ivashchenko <likemusicdev@gmail.com>
2020-03-12 00:58:26 +0300

    Fix createCar()

--- tests/Textures/PostData/Driver.php
+++ tests/Textures/PostData/Driver.php
@@ -1,36 +1,31 @@
 <?php
 
 return [
-    'accounts' =>
-        [
-            'balance_limit' => '5',
-        ],
-    'driver_profile' =>
-        [
-            'driver_license' =>
-                [
-                    'country' => 'rus',
-                    'number' => 'OA111534', //overridden
-                    'expiration_date' => '2021-01-13',
-                    'issue_date' => '2011-01-13',
+    'accounts' => [
+        'balance_limit' => '5',
+    ],
+    'driver_profile' => [
+        'driver_license' => [
+            'country' => 'rus',
+            'number' => 'OA111534', //overridden
+            'expiration_date' => '2021-01-13',
+            'issue_date' => '2011-01-13',
 //                        'birth_date' => null,
 //                        'birth_date' => '1985-02-01',
-                ],
-            'first_name' => 'Валерий',
-            'last_name' => 'Иващенко',
-            'middle_name' => 'Игоревич',
-            'phones' => //overridden
-                [
-                    '+375333012955',
+        ],
+        'first_name' => 'Валерий',
+        'last_name' => 'Иващенко',
+        'middle_name' => 'Игоревич',
+        'phones' => [ //overridden
+            '+375333012955',
 //                            '+7 (753) 330-12-95',
-                ],
-            'work_rule_id' => 'bea83f1024f94dfd980a97a26705f070', //overridden
-            'balance_deny_onlycard' => false,
-            'providers' =>
-                [
-                    0 => 'yandex',
-                ],
-            'hire_date' => '2019-09-01', //overridden
+        ],
+        'work_rule_id' => 'bea83f1024f94dfd980a97a26705f070', //overridden
+        'balance_deny_onlycard' => false,
+        'providers' => [
+            0 => 'yandex',
+        ],
+        'hire_date' => '2019-09-01', //overridden
 //                    'deaf' => NULL,
 //                    'email' => NULL,
 //                    'address' => NULL,
@@ -43,5 +38,5 @@ return [
 //                    'identifications' => [],
 //                    'primary_state_registration_number' => null,
 //                    'tax_identification_number' => null,
-        ],
+    ],
 ];
